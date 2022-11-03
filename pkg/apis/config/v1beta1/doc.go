8946ef2c644212496b95e474090f9c048aea440d
wohdauhf <10334369+wohdauhf@user.noreply.gitee.com>
2022-11-04 00:10:43 +0800

    update tag

--- pkg/apis/config/v1beta1/doc.go
+++ pkg/apis/config/v1beta1/doc.go
@@ -1,6 +1,6 @@
 // +k8s:deepcopy-gen=package
 // +k8s:conversion-gen=sigs.k8s.io/scheduler-plugins/pkg/apis/config
-// +k8s:defaulter-gen=TypeMeta
+// +k8s:deepcopy-gen=package,register
 // +groupName=kubescheduler.config.k8s.io
 
 //package v1beta1是API的v1beta1 version
